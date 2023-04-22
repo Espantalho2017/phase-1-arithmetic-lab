0709b0ebbc0dacc1effb8a6b960e49d56b8b8c7f
Roscoe Moore <rmoore@peersat.com>
2023-04-21 22:49:36 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let multiply;
+const num1 = 2;
+const num2 = 31;
+multiply = num1 * num2;
+
+let random;
+random = Math.ceil(Math.random()); 
+
+let mod;
+const num3 = 25;
+const num4 = 7;
+mod = num3 % num4;
+
+let max;
+max = Math.max(1, 2, 4, 20);
